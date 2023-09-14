7cc7721c35cc4c13d16d1e03259f90a2df586132
markande98 <gauravbpit1998@gmail.com>
2023-09-14 11:17:08 +0530

    auth modal completed

--- components/AuthModal.tsx
+++ components/AuthModal.tsx
@@ -1,16 +1,62 @@
-'use client';
+"use client";
 
+import {
+  useSessionContext,
+  useSupabaseClient,
+} from "@supabase/auth-helpers-react";
 import Modal from "./Modal";
+import { useRouter } from "next/navigation";
+import { Auth } from "@supabase/auth-ui-react";
+import { ThemeSupa } from "@supabase/auth-ui-shared";
+import useAuthModal from "@/hooks/useAuthModal";
+import { useEffect } from "react";
 
 const AuthModal = () => {
-    return ( <Modal
-        title="Welcome back"
-        description="Login to your account"
-        isOpen
-        onChange={() => {}}
+  const supabaseClient = useSupabaseClient();
+  const router = useRouter();
+  const { session } = useSessionContext();
+
+  const { onClose, isOpen } = useAuthModal();
+
+  useEffect(() => {
+    if (session) {
+      router.refresh();
+      onClose();
+    }
+  }, [router, onClose, session]);
+
+  const onChange = (open: boolean) => {
+    if (!open) {
+      onClose();
+    }
+  };
+
+  return (
+    <Modal
+      title="Welcome back"
+      description="Login to your account"
+      isOpen={isOpen}
+      onChange={onChange}
     >
-        Auth Modal Children
-    </Modal> );
-}
- 
+      <Auth
+        magicLink
+        providers={["github"]}
+        theme="dark"
+        supabaseClient={supabaseClient}
+        appearance={{
+          theme: ThemeSupa,
+          variables: {
+            default: {
+              colors: {
+                brand: "#404040",
+                brandAccent: "#22c55e",
+              },
+            },
+          },
+        }}
+      />
+    </Modal>
+  );
+};
+
 export default AuthModal;
