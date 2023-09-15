c48b3926a5d202d0e5e59caf6d1f787e20872a09
markande98 <gauravbpit1998@gmail.com>
2023-09-15 09:04:56 +0530

    user song library ui working

--- components/Library.tsx
+++ components/Library.tsx
@@ -5,8 +5,15 @@ import { AiOutlinePlus } from "react-icons/ai";
 import useAuthModal from "@/hooks/useAuthModal";
 import { useUser } from "@/hooks/useUser";
 import useUploadModal from "@/hooks/useUploadModal";
+import { Song } from "@/types";
+import MediaItem from "./MediaItem";
 
-const Library = () => {
+interface LibraryProps{
+  songs: Song[]
+}
+const Library: React.FC<LibraryProps> = ({
+  songs
+}) => {
   const authModal = useAuthModal();
   const uploadModal = useUploadModal();
   const { user } = useUser();
@@ -53,7 +60,13 @@ const Library = () => {
         px-3
       "
       >
-        List of Songs!
+        {songs.map(item => (
+          <MediaItem 
+            key={item.id}
+            data={item}
+            onClick={() => {}}
+          />
+        ))}
       </div>
     </div>
   );
